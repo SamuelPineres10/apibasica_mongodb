7172a8b7c5054be1dff354d85daa56b26534b168
SamuelPineres10 <samuelpineres.rios10@gmail.com>
2026-06-04 10:22:56 -0500

    'CAMBIOS'

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const serviceController = require('./controllers/servicio.controller')
 
-
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
 
 
 const port = process.env.PORT || 9999
@@ -36,5 +34,4 @@ app.delete('/servicios/:id', serviceController.deleteServiceOne)
 
 app.listen(port, () => {
     console.log(`Estas escuchando http://localhost:${port}`);
-
 })
